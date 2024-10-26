1a9f153879e185722ef04c77456485f34586eeb8
KimchiBee19 <c14220182@john.petra.ac.id>
2024-10-26 16:31:42 +0700

    update route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\BookingController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -15,8 +16,18 @@ Route::get('/service', function () {
     return view('service');
 });
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::get('/home/book/{service:id}', [BookingController::class, 'showBookingForm'])->name('book.showBookingForm');
 
 Route::get('/staff/available', [BookingController::class, 'availableStaff'])->name('staff.available');
 
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
